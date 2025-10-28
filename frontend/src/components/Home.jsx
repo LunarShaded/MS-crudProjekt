328936053f3dd2618783b6345be807a5949a3a31
Maksym Shevchyk <maksim.shevchik@gmail.com>
2025-10-28 15:36:54 +0100

    Naprawiono błąd

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-
+const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
 const Home = () => {
   const [appInfo, setAppInfo] = useState(null)
